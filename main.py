1fcf01a5206a531c3d856edc1a9d5ca7b6ba65f0
Dark Soulz <49095440+jupiterdv@users.noreply.github.com>
2020-08-01 17:05:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,117 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from tkinter import *
+import pyttsx3 as pp
+import speech_recognition as s
+import threading
+
+engine = pp.init()
+
+voices = engine.getProperty('voices')
+print(voices)
+
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(words):
+    engine.say(words)
+    engine.runAndWait()
+
+
+# pyttsx3
+bot = ChatBot("T.Codezine Bot")
+
+convo = open('chat.txt', 'r').readlines()
+
+trainer = ListTrainer(bot)
+
+# now training the bot with the help of trainer
+
+trainer.train(convo)
+
+
+
+main = Tk()
+
+main.geometry("500x650")
+
+main.title("T.Codezine bot")
+
+img = PhotoImage(file="bot.png")
+
+photoL = Label(main, image=img)
+
+photoL.pack(pady=5)
+
+
+# takey query : it takes audio as input from user and convert it to string..
+
+def takeQuery():
+    sr = s.Recognizer()
+    sr.pause_threshold = 1
+    print("your bot is listening try to speak")
+    with s.Microphone() as m:
+        try:
+            audio = sr.listen(m)
+            query = sr.recognize_google(audio, language='eng-in')
+            print(query)
+            textF.delete(0, END)
+            textF.insert(0, query)
+            ask_from_bot()
+        except Exception as e:
+            print(e)
+            print("not recognized")
+
+
+def ask_from_bot():
+    query = textF.get()
+    answer_from_bot = bot.get_response(query)
+    msgs.insert(END, "you : " + query)
+    print(type(answer_from_bot))
+    msgs.insert(END, "c.z bot : " + str(answer_from_bot))
+    speak(answer_from_bot)
+    textF.delete(0, END)
+    msgs.yview(END)
+
+
+frame = Frame(main)
+
+sc = Scrollbar(frame)
+msgs = Listbox(frame, width=80, height=20, yscrollcommand=sc.set)
+
+sc.pack(side=RIGHT, fill=Y)
+
+msgs.pack(side=LEFT, fill=BOTH, pady=10)
+
+frame.pack()
+
+# creating text field
+
+textF = Entry(main, font=("Courier", 10))
+textF.pack(fill=X, pady=10)
+
+btn = Button(main, text="Ask T.Codezine bot", font=(
+    "Courier", 10),bg='red', command=ask_from_bot)
+btn.pack()
+
+
+# creating a function
+def enter_function(event):
+    btn.invoke()
+
+
+# going to bind main window with enter key...
+
+main.bind('<Return>', enter_function)
+
+
+def repeatL():
+    while True:
+        takeQuery()
+
+
+t = threading.Thread(target=repeatL)
+
+t.start()
+
+main.mainloop()
